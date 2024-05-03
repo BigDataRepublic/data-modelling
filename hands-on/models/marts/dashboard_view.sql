36a1718af60913562a58a20699046bee4ad15ebb
Sam Sweere <samsweere@gmail.com>
2024-05-03 13:56:22 +0200

    created final data view

---
+++ hands-on/models/marts/dashboard_view.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized="view",
+    )
+}}
+
+WITH _base_session_facts AS (
+    SELECT 
+        sd.Subject,
+        sd.Topic,
+        AVG(sf.YearsOfExperience) AS AvgYearsOfExperience
+    FROM {{ref('session_facts')}} sf
+    JOIN {{ref('session_dim')}} sd
+    ON sf.SessionID = sd.SessionID
+    GROUP BY sd.Subject, sd.Topic
+)
+
+SELECT 
+    bsf.Subject,
+    bsf.Topic,
+    bsf.AvgYearsOfExperience
+FROM _base_session_facts bsf
